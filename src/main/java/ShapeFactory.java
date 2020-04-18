d856af4a32648c208bc7ba2a690af9367f294dd5
Oleh Kuzmenko <oleg24453@gmail.com>
2020-04-18 17:04:39 +0300

    added correction

--- src/main/java/ShapeFactory.java
+++ src/main/java/ShapeFactory.java
@@ -1,28 +1,33 @@
 import java.util.Random;
 
 public class ShapeFactory {
+
     public static Shape getShape(String shapeType) {
         if (shapeType == null) {
             return null;
         }
         if (shapeType.equalsIgnoreCase("трапеція")) {
-            return  new Trapeze("Трапеція",
-                    Colors.values()[new Random().nextInt(Colors.values().length)], new Random().nextInt(25) + 1,
+            return new Trapeze("Трапеція",
+                    Colors.values()[new Random().nextInt(Colors.values().length)],
+                    new Random().nextInt(25) + 1,
                     new Random().nextInt(25) + 1, new Random().nextInt(25) + 1);
         }
         if (shapeType.equalsIgnoreCase("трикутник")) {
             return new Triangle("Трикутник",
-                    Colors.values()[new Random().nextInt(Colors.values().length)], new Random().nextInt(25) + 1,
+                    Colors.values()[new Random().nextInt(Colors.values().length)],
+                    new Random().nextInt(25) + 1,
                     new Random().nextInt(25) + 1);
         }
         if (shapeType.equalsIgnoreCase("коло")) {
-            return new Circle("Коло", Colors.values()[new Random().nextInt(Colors.values().length)],
+            return new Circle("Коло",
+                    Colors.values()[new Random().nextInt(Colors.values().length)],
                     new Random().nextInt(25) + 1);
         }
         if (shapeType.equalsIgnoreCase("квадрат")) {
-            return new Quadrate("Квадрат", Colors.values()[new Random().nextInt(Colors.values().length)],
+            return new Quadrate("Квадрат",
+                    Colors.values()[new Random().nextInt(Colors.values().length)],
                     new Random().nextInt(25) + 1);
         }
-    return null;
+        return null;
     }
 }
